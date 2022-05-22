2df823ff260ac79d7f52a3ad8939464387de997a
unknown <cholito9313@gmail.com>
2022-05-22 20:17:25 +0300

    message enstead alert

--- main.js
+++ main.js
@@ -4,30 +4,23 @@ function checkForm() {
     let age = document.getElementById("age");
     let email = document.getElementById("email");
     let password = document.getElementById("password");
-    let text = document.getElementById("text")
-    let form = document.getElementById("form")
+    let text = document.getElementById("text");
+    let form = document.getElementById("form");
 
     if (name.value == '') {
-        alert ("Please, enter your name");
+        document.getElementbyId('enterName').innerHTML += "Please, enter your name";
+    } else if (surname.value == '') {
+        alert("Please, enter your surname");
+    } else if (age.value == '') {
+        alert("Please, enter your age");
+    } else if (email.value == '') {
+        alert("Please, enter email adress");
+    } else if (password.value == '') {
+        alert("Please, enter password");
+    } else if (password.value.length < 6) {
+        alert("Password length should be not less than 6 sympols");
+    } else {
+        text.classList.remove("textNonRegistered");
+        form.classList.add("registered");
+    }
 }
-    else if (surname.value == '') {
-        alert ("Please, enter your surname");
-    }
-   else if (age.value == '') {
-   alert ("Please, enter your age");
-   }
-   else if (email.value == '') {
-    alert ("Please, enter email adress");
-    }
-    else if (password.value ==''){
-        alert ("Please, enter password");
-    }
-    else if (password.value.length < 6){
-        alert ("Password length should be not less than 6 sympols");
-    }
-else {
-text.classList.remove("textNonRegistered");
-form.classList.add("registered");
-}
-}
-
